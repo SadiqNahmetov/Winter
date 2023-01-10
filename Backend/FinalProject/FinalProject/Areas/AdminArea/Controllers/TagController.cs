9655af7409dae31ccf6397fed102d4237550b07b
SadiqNahmetov <sadigfn@code.edu.az>
2023-01-10 14:10:19 +0400

    Tag

---
+++ Backend/FinalProject/FinalProject/Areas/AdminArea/Controllers/TagController.cs
@@ -0,0 +1,109 @@
+﻿using FinalProject.Data;
+using FinalProject.Helpers;
+using FinalProject.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FinalProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class TagController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly IWebHostEnvironment _env;
+
+        public TagController(AppDbContext context, IWebHostEnvironment env)
+        {
+            _context = context;
+            _env = env;
+        }
+        public async Task<IActionResult> Index()
+        {
+            IEnumerable<Tag> tag = await _context.Tags.Where(m => !m.IsDeleted).ToListAsync();
+            return View(tag);
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Tag tag)
+        {
+            if (!ModelState.IsValid) return View();
+
+
+           
+            await _context.Tags.AddAsync(tag);
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
+
+
+
+
+        [HttpGet]
+        public async Task<IActionResult> Detail(int? id)
+        {
+            if (id == null) return BadRequest();
+
+            Tag tag = await _context.Tags.FindAsync(id);
+
+            if (tag == null) return NotFound();
+
+            return View(tag);
+        }
+
+
+       
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            Tag tag = await _context.Tags
+                .Where(m => !m.IsDeleted && m.Id == id)
+                .FirstOrDefaultAsync();
+
+            if (tag == null) return NotFound();
+
+
+            string path = Helper.GetFilePath(_env.WebRootPath, "img", tag.Name);
+            Helper.DeleteFile(path);
+            tag.IsDeleted = true;
+
+
+            tag.IsDeleted = true;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+
+        }
+
+
+
+
+        private async Task<Brand> GetByIdAsync(int id)
+        {
+            return await _context.Brands.FindAsync(id);
+        }
+    }
+}
